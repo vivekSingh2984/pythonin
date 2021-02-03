f6d446301da32be31478c6af15892bc3b45a3c37
Vivek Singh <vivek_singh2984@hotmail.com>
2021-02-03 14:18:31 +0000

    remove folder

--- PythonClouds/bin/app.publish/AppJs/Controllers/UsersController.js
+++
@@ -1,7 +0,0 @@
-﻿/// <reference path="../Directives/uiHeaderView.js" />
-define(['app', 'Directives/uiHeaderView'], function (app) {
-    app.controller('UsersController', function ($scope, $location, $window, $route, $filter, ngToast, $sce, blockUI, $sessionStorage, $routeParams, $websocket, NotificationWebSocket) {
-
-
-    });
-});
--- PythonClouds/bin/app.publish/AppJs/Controllers/python/PythonController.js
+++
@@ -1,5 +0,0 @@
-﻿define(['app', 'Directives/uiHeaderView', 'Directives/uiFooterView', 'Directives/uiSideView', 'factory/HomeServices', 'factory/AccountServices'], function (app) {
-    app.controller('PythonController', function ($scope, $sessionStorage, $location, $timeout, $route, ngToast, $sce, blockUI, HomeServices, AccountServices) {
-         
-    });
-});
